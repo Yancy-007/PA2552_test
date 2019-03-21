6d0a4bee939584d78bf48d6dc03a40e90d44d199
Yancy_007 <Yancy1995@outlook.com>
2019-03-21 18:00:45 +0100

    Test7

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -11,7 +11,6 @@ public class CalculatorMain {
 
 	public int mul(int a, int b) { return a*b; }
 	//public int div(int a, int b) {return a/b; }
-
 	public int div(int a, int b) {
 		if(b == 0) { return 0;}
 	return a/b; }
